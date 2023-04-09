e56fde9fe6e82e0f492f1ce933fa46daaf8e069f
Tihomir Trifonov <t.a.trifonov@gmail.com>
2023-04-09 20:37:29 +0300

    Typescript store finished

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,6 +1,9 @@
 import { createSelector } from 'reselect';
+import { CategoriesState } from './category.reducer';
+import { CategoryMap } from './category.types';
+import { RootState } from '../store';
 
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state:RootState): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -10,11 +13,11 @@ export const selectCategories = createSelector(
 export const selectCategoriesMap = createSelector(
   [selectCategories],
   (categories) =>
-    categories.reduce((acc, category) => {
+    categories.reduce((acc, category): CategoryMap => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
